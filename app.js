3a3a49e727d3b4d82061644b3ccc9dabd1a77c97
Maximilian Schwarzmüller <company@academind.com>
2018-06-27 08:47:48 +0200

    finished advanced modal

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var buttons = document.querySelectorAll('button');
-var modal = document.querySelector('.modal');
-var backdrop = document.querySelector('.backdrop');
+var editValueButton = document.querySelector('button');
+var demoContainer = document.querySelector('.demo-container');
+var backdrop;
+var modal;
 
 var outputParagraph = document.querySelector('#quote');
-var textEdit = document.querySelector('.modal textarea');
+// var textEdit = document.querySelector('.modal textarea');
 
 var quote = 'A Modal Component on a Webpage - how hard could it be?';
 var editedQuote = '';
@@ -13,30 +14,83 @@ function updateParagraph() {
 }
 
 function closeModal() {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  if (backdrop) {
+    backdrop.remove();
+  }
+
+  if (modal) {
+    modal.remove();
+  }
 }
 
 updateParagraph();
 
-buttons[2].addEventListener('click', function() {
-  modal.style.display = 'block';
-  backdrop.style.display = 'block';
+editValueButton.addEventListener('click', function() {
+  backdrop = document.createElement('div');
+  backdrop.classList.add('backdrop');
+  backdrop.addEventListener('click', closeModal);
+  document.body.insertBefore(backdrop, demoContainer);
+  backdrop.addEventListener('click', closeModal);
+
+  modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  var modalHeading = document.createElement('h1');
+  modalHeading.textContent = 'Edit your Statement';
+  modal.appendChild(modalHeading);
+
+  var textEditContainer = document.createElement('div');
+  textEditContainer.classList.add('modal-input');
+  modal.appendChild(textEditContainer);
+
+  var textEditArea = document.createElement('textarea');
+  textEditArea.rows = '3';
+  textEditArea.addEventListener('input', function() {
+    editedQuote = textEditArea.value;
+  });
+  textEditArea.value = quote;
+  textEditContainer.appendChild(textEditArea);
+
+  var modalActionsContainer = document.createElement('div');
+  modalActionsContainer.classList.add('modal-actions');
+  modal.appendChild(modalActionsContainer);
+
+  var cancelButton = document.createElement('button');
+  cancelButton.setAttribute('type', 'button');
+  cancelButton.classList.add('btn-cancel');
+  cancelButton.textContent = 'Cancel';
+  cancelButton.addEventListener('click', closeModal);
+  modalActionsContainer.appendChild(cancelButton);
+
+  var confirmButton = document.createElement('button');
+  confirmButton.setAttribute('type', 'button');
+  confirmButton.classList.add('btn-confirm');
+  confirmButton.textContent = 'Confirm';
+  confirmButton.addEventListener('click', function() {
+    closeModal();
+    if (editedQuote.trim().length > 0) {
+      quote = editedQuote;
+      updateParagraph();
+    }
+  });
+  modalActionsContainer.appendChild(confirmButton);
+
+  document.body.insertBefore(modal, demoContainer);
+
   editedQuote = quote;
-  textEdit.value = quote;
+  //   textEdit.value = quote;
 });
 
-buttons[0].addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
+// buttons[0].addEventListener('click', closeModal);
 
-buttons[1].addEventListener('click', function() {
-  closeModal();
-  if (editedQuote.trim().length > 0) {
-    quote = editedQuote;
-    updateParagraph();
-  }
-});
+// buttons[1].addEventListener('click', function() {
+//   closeModal();
+//   if (editedQuote.trim().length > 0) {
+//     quote = editedQuote;
+//     updateParagraph();
+//   }
+// });
 
-textEdit.addEventListener('input', function() {
-  editedQuote = textEdit.value;
-});
+// textEdit.addEventListener('input', function() {
+//   editedQuote = textEdit.value;
+// });
